d233ee6c332be0bfc0e3cd6c1a05347a1ca522dc
fiopwk <flopwiki@gmail.com>
2020-02-20 05:31:20 -0500

    added and imported mutationobserver-shim

--- src/components/contactform.test.js
+++ src/components/contactform.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, getByText } from '@testing-library/react';
 import ContactForm from "./ContactForm";
+import 'mutationobserver-shim';
 
 const contactForm = require('./ContactForm');
 test('renders without error', () => {
+   //Arrange
    const { getByText } = render(<ContactForm/>);
+
+//   Act
+
+//   Assert
+
+
 });
