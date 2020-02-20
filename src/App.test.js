7a89dee46670de36d357034b5ad66ba1c22d2ff8
fiopwk <flopwiki@gmail.com>
2020-02-20 09:13:44 -0500

    added 3 simple test to verify working setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, cleanup  } from "@testing-library/react";
 import App from "./App";
 import 'mutationobserver-shim';
+import ContactForm from "./components/ContactForm";
+import '@testing-library/jest-dom/extend-expect';
+import axiosMock from 'axios';
+import {
+  findAllByPlaceholderText,
+  getAllByAltText,
+  getAllByPlaceholderText, getByLabelText,
+  getByPlaceholderText,
+  getByText, getAllByLabelText
+} from "@testing-library/dom";
+
 
 test("renders App without crashing", () => {
   render(<App />);
+});
+
+test("renders ContactForm without crashing", () => {
+  //Arrange
+ render(<ContactForm/>);
+
+});
+
+test('name field accepts a max of 3 characters', () => {
+//  Arrange
+  const {getByRole } = render (<App/>);
+
+  // Act
+
+//  Assert
 });
